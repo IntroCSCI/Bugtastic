1bf67e637d4f082f09ddded834d7909a59c99c6b
Preston Massey-Blake <pmassey-blake@mail.csuchico.edu>
2022-02-07 12:28:20 -0800

    gwg

--- main.cpp
+++ main.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
-
+#include <string>
 int main()
 {
   int age;
-  string name;
-  char last initial;
+  char name;
+  char lastinitial;
 
-  cout<<"Hi. What is your first name? "
-  cin<<name;
+  cout<<"Hi. What is your first name? ";
+  cin>>name;
   cout<<"name, what is the first letter of your last name? ";
-  cin>>last initial;
-  cout<<"Thanks"<<name<< <<last initial<<.<<endl;
+  cin>>lastinitial;
+  cout<<"Thanks"<<name<< <<lastinitial<<.<<endl;
   cout<<"Please also tell me how old you are: ";
-  cin>>age>>endl;
+  cin>>age;
 
   if( age < 12 )
   {
     cout<<"Hey kid, how do you like school?\n";
   }
-  if( age < 18 )
-    cout<<"Cool!"endl;
+  if( age < 18 ){
+    cout<<"Cool!";
     cout<<"How's highschool going?\n";
-  else( age >= 18 )
+  }
+  else if( age >= 18 )
   {
     cout<<"Pleased to meet you!\n";
   }
