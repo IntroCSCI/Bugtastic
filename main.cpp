07fd1fa75f61033f2889b0f7c10afcc82e2dbc13
Andrew_Nave <anave1@mail.csuchico.edu>
2022-02-07 11:49:00 -0800

    fixed CS111 bugs

--- main.cpp
+++ main.cpp
@@ -9,20 +9,21 @@ int main()
 
   cout<<"Hi. What is your first name? "
   cin<<name;
-  cout<<"name, what is the first letter of your last name? ";
+  cout<<name<<", what is the first letter of your last name? ";
   cin>>last initial;
   cout<<"Thanks, "<<name<< <<last initial<<.<<endl;
   cout<<"Please also tell me how old you are: ";
-  cin>>age>>endl;
+  cin>>age;
+  cout<<endl;
 
   if( age < 12 )
   {
     cout<<"Hey kid, how do you like school?\n";
   }
-  if( age < 18 )
-    cout<<"Cool!"endl;
+  else if( age < 18 )
+    cout<<"Cool!\n";
     cout<<"How's highschool going?\n";
-  else( age >= 18 );
+  else if( age >= 18 )
   {
     cout<<"Pleased to meet you!\n";
   }
