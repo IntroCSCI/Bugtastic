287fd833aeaa90783135daa92aa57a7facb70e07
SGurmanjeet22 <gsingh2@mail.csuchico.edu>
2022-02-07 11:37:11 -0800

    Fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
@@ -8,12 +9,14 @@ int main()
   char last initial;
 
   cout<<"Hi. What is your first name? "
-  cin<<name;
-  cout<<"name, what is the first letter of your last name? ";
+  getline(cin,name);
+  cin.ignore();
+  cout<<name<<", what is the first letter of your last name? ";
   cin>>last initial;
-  cout<<"Thanks, "<<name<< <<last initial<<.<<endl;
+  cout<<"Thanks, "<<name<< <<last initial<<i"."<<endl;
   cout<<"Please also tell me how old you are: ";
-  cin>>age>>endl;
+  cin>>age;
+  cout << endl;
 
   if( age < 12 )
   {
