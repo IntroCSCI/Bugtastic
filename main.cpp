4f234e64a7c8ae35f3a41d7924ada1e4f41d6db3
Braden <braden@bradenn.com>
2022-02-07 11:38:06 -0800

    Fixed silly errors

--- main.cpp
+++ main.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-  int age;
-  string name;
-  char last initial;
+int main() {
+    int age;
+    string name;
+    char lastInitial;
 
-  cout<<"Hi. What is your first name? "
-  cin<<name;
-  cout<<"name, what is the first letter of your last name? ";
-  cin>>last initial;
-  cout<<"Thanks, "<<name<< <<last initial<<.<<endl;
-  cout<<"Please also tell me how old you are: ";
-  cin>>age>>endl;
+    cout << "Hi. What is your first name? " << endl;
+    cin >> name;
 
-  if( age < 12 )
-  {
-    cout<<"Hey kid, how do you like school?\n";
-  }
-  if( age < 18 )
-    cout<<"Cool!"endl;
-    cout<<"How's highschool going?\n";
-  else( age >= 18 );
-  {
-    cout<<"Pleased to meet you!\n";
-  }
+    cout << name << ", what is the first letter of your last name? ";
+    cin >> lastInitial;
 
-  return 0;
+    cout << "Thanks, " << name << " " << lastInitial << "." << endl;
+    cout << "Please also tell me how old you are: ";
+    cin >> age;
+
+    if (age < 12) {
+        cout << "Hey kid, how do you like school?\n";
+    }else if(age < 18) {
+        cout << "Cool!" << endl;
+        cout << "How's highschool going?\n";
+    } else if (age >= 18) {
+        cout << "Pleased to meet you!\n";
+    }
+
+    return 0;
 }
