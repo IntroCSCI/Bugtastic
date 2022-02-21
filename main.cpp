276a71214958645f1b53f2152b6c1e82152253d0
mwkuse <91228185+mwkuse@users.noreply.github.com>
2022-02-21 02:43:19 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main()
     cout<<"Hey kid, how do you like school?\n";
   }
   else if( age < 18 ){
-    cout<<"Cool!"endl;
+    cout<<"Cool!"<<endl;
     cout<<"How's highschool going?\n";
   }
   else if( age == 18 )
