746ed8dfdac4d86fc203de90249f39b6c51120dd
mwkuse <91228185+mwkuse@users.noreply.github.com>
2022-02-21 02:44:58 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main()
   cin >> name;
   cout << name ", what is the first letter of your last name? ";
   cin >> last_initial;
-  cout << "Thanks, " << name << " " << last initial << "." <<endl;
+  cout << "Thanks, " << name << " " << last_initial << "." <<endl;
   cout<<"Please also tell me how old you are: ";
   cin>>age;
 
